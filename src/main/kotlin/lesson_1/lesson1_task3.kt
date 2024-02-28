7fa24016a63abae1385f0e18ed4cfee25d254239
rnh-1 <rnh-1981@mail.ru>
2024-02-28 16:57:38 +0400

    KS-1-3-variable

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_1
+
+fun main() {
+    val year = 1961
+    val hour = "09"
+    val minute = "07"
+    var hourMinute = "10:55"
+    println(year)
+    println(hour)
+    println(minute)
+    println(hourMinute)
+
+}
