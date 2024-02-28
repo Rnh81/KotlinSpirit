be50ec77da255b73531cd9357537d40bc588594e
rnh-1 <rnh-1981@mail.ru>
2024-02-28 14:01:48 +0400

    KS-1-2 code refactor

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_1
+
+fun main(){
+    val numberSneakrs = 75
+    val notificationText = "Благодарим за покупку!"
+    println(numberSneakrs)
+    println(notificationText)
+
+    val workersNum = 2000
+    val theDepartedNum = 1
+
+    println(workersNum - theDepartedNum)
+
+
+}
