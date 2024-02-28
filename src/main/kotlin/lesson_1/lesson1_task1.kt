e12b3edbbca2d2b03f3730eb142e7018ace21c5a
rnh-1 <rnh-1981@mail.ru>
2024-02-28 13:22:18 +0400

    KS-1-1 code refactor

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,5 +1,9 @@
 package org.example.lesson_1
 
 fun main() {
-    println("Решение 1")
+    val numberSneakrs = 75
+    val notificationText = "Благодарим за покупку!"
+    println(numberSneakrs)
+    println(notificationText)
+
 }
