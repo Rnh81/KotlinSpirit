80f6aa95c5078506e4afe407e7529eca5590fef5
rnh-1 <rnh-1981@mail.ru>
2024-02-29 09:51:40 +0400

    KS-1-2-refactor

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -4,7 +4,7 @@ fun main() {
     val year = 1961
     val hour = "09"
     val minute = "07"
-    var hourMinute = "10:55"
+    val hourMinute = "10:55"
     println(year)
     println(hour)
     println(minute)
